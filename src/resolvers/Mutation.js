60cf4550784bcbd267083fe991c985a00b1a1cda
matheusps <matheus.ps@icloud.com>
2019-03-14 17:17:09 -0300

    Added prettier, Added login mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,16 +1,31 @@
-import bcrypt  from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { APP_SECRET } from '../utils'
 
 const signup = async (parent, args, context, info) => {
-    const password = await bcrypt.hash(args.password, 10)
-    const user = await context.prisma.createUser({...args, password})
-    const token = jwt.sign({ userId: user.id }, APP_SECRET)
-    
-    return {
-        token,
-        user,
-    }
+  const password = await bcrypt.hash(args.password, 10)
+  const user = await context.prisma.createUser({ ...args, password })
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
 }
 
-export { signup }
+const login = async (parent, args, context, info) => {
+  const user = await context.prisma.user({ email: args.email })
+  if (!user) throw new Error('No such user found! ☹️')
+
+  const valid = await bcrypt.compare(args.password, user.password)
+  if (!valid) throw new Error('Invalid password! ☹️')
+
+  const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+  return {
+    token,
+    user,
+  }
+}
+
+export { signup, login }
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
-export const APP_SECRET = 'f1t-l1f3-**@#$^&$**-v3ry-h4rd-S3cR3t'
+const APP_SECRET = 'f1t-l1f3-**@#$^&$**-v3ry-h4rd-S3cR3t'
+
+const getUserId = context => {
+  const Authorization = context.request.get('Authorization')
+  if (!Authorization) throw new Error('Not authenticated')
+
+  const token = Authorization.replace('Bearer', '')
+  const { userId } = jwt.verify(token, APP_SECRET)
+
+  return userId
+}
+
+export { APP_SECRET, getUserId }
